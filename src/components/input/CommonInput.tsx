e374cbed39483f0f24f9e418927cc48416db81cd
Gvvon5ewon <gwonsewon03@gmail.com>
2023-12-07 08:26:14 +0900

    Feat: new property, type

--- src/components/input/CommonInput.tsx
+++ src/components/input/CommonInput.tsx
@@ -2,21 +2,22 @@ import React from "react";
 import styled from "styled-components";
 
 interface InputPropsType extends React.InputHTMLAttributes<HTMLInputElement> {
-  width?: string;
-  height?: string;
+    width?: string;
+    height?: string;
+    margin?: string;
 }
 
-const CommonInput = ({ width, height, ...rest }: InputPropsType) => {
-  return <InputBox style={{ width, height }} {...rest} />;
+const CommonInput = ({ width, height, margin, ...rest }: InputPropsType) => {
+    return <InputBox style={{ width, height, margin }} {...rest} />;
 };
 
 const InputBox = styled.input`
-  border-radius: 3px;
-  background-color: #202225;
-  border: none;
-  color: white;
-  font-size: 15px;
-  padding: 3%;
+    border-radius: 3px;
+    background-color: #202225;
+    border: none;
+    color: white;
+    font-size: 15px;
+    padding: 3%;
 `;
 
 export default CommonInput;
