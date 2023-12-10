67c7605c6fd219954d13ffde07c51a55f34e738f
5ewon06 <gwonsewon03@gmail.com>
2023-12-10 10:28:02 +0900

    Add: Delete, Join Modal 추가

--- src/page/Main/layout/index.tsx
+++ src/page/Main/layout/index.tsx
@@ -7,29 +7,37 @@ import SettingModal from "@/components/modal/SettingModal";
 import CreateModal from "@/components/modal/CreateModal";
 import settingModalStore from "@/store/settingModal.store";
 import createModal from "@/store/createModal.store";
+import deleteModal from "@/store/deleteModal.store";
+import DeleteModal from "@/components/modal/DeleteModal";
+import JoinModal from "@/components/modal/JoinModal";
+import joinModal from "@/store/joinModal.store";
 
 const MainPage = () => {
-    const [isModal] = useRecoilState(settingModalStore);
-    const [isModal2] = useRecoilState(createModal);
-    return (
-        <Container>
-            {isModal ? <SettingModal /> : ""}
-            {isModal2 ? <CreateModal /> : ""}
-            <MyCondition />
-            <MyGroup />
-        </Container>
-    );
+  const [isModal] = useRecoilState(settingModalStore);
+  const [isModal2] = useRecoilState(createModal);
+  const [isModal3] = useRecoilState(deleteModal);
+  const [isModal4] = useRecoilState(joinModal);
+  return (
+    <Container>
+      {isModal ? <SettingModal /> : ""}
+      {isModal2 ? <CreateModal /> : ""}
+      {isModal3 ? <DeleteModal /> : ""}
+      {isModal4 ? <JoinModal /> : ""}
+      <MyCondition />
+      <MyGroup />
+    </Container>
+  );
 };
 
 const Container = styled.div`
-    width: 100%;
-    height: 100vh;
-    background-color: #717bfe;
+  width: 100%;
+  height: 100vh;
+  background-color: #717bfe;
 
-    overflow-y: hidden;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  overflow-y: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 export default MainPage;
