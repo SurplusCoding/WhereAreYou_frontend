90a2d75c25bf1806b3bc8ba1ac36dceb924a6fae
5ewon06 <gwonsewon03@gmail.com>
2023-12-10 10:16:40 +0900

    Add: delete Modal

---
+++ src/components/modal/DeleteModal.tsx
@@ -0,0 +1,107 @@
+import Image from "next/image";
+import styled from "styled-components";
+import CancelIcon from "@/page/Main/assets/cancelIcon.svg";
+import { useRecoilState } from "recoil";
+import React from "react";
+import deleteModal from "@/store/deleteModal.store";
+import CommonButton from "../button/CommonButton";
+import { deleteGroup } from "@/api";
+import currentGroupStore from "@/store/currentGroup.store";
+
+const DeleteModal = () => {
+  const [isModal, setIsModal] = useRecoilState(deleteModal);
+  const [currentGroup] = useRecoilState(currentGroupStore);
+
+  const hadleDeleteGroup = async (teamId: number) => {
+    const result = await deleteGroup(teamId);
+    if (result.success === false) return alert("그룹 삭제에 실패하였습니다.");
+    alert("그룹 삭제에 성공하였습니다.");
+    setIsModal(false);
+  };
+
+  const handleChildClick = (event: React.MouseEvent) => {
+    event.stopPropagation();
+  };
+
+  return (
+    <Wrapper onClick={() => setIsModal(false)}>
+      <ModalBox onClick={handleChildClick}>
+        <ModalHeader>
+          <ModalText>Delete Group</ModalText>
+          <Cancel
+            src={CancelIcon}
+            alt="CANCEL_ICON"
+            onClick={() => setIsModal(false)}
+          />
+        </ModalHeader>
+        <ModalContent>
+          <ModalText>
+            그룹 {currentGroup.name} 를/을 삭제하시겠습니까?
+          </ModalText>
+          <CommonButton
+            width="90%"
+            height="15%"
+            margin="auto auto 2vh auto"
+            onClick={() => hadleDeleteGroup(currentGroup.id)}
+          >
+            Delete Group
+          </CommonButton>
+        </ModalContent>
+      </ModalBox>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: #00000032;
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1;
+`;
+
+const ModalBox = styled.div`
+  width: 45vw;
+  height: 55vh;
+  background-color: #3e4249;
+  border-radius: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const ModalHeader = styled.div`
+  width: 100%;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  border-radius: 5px 5px 0 0;
+  border-bottom: 2px solid #2d3034;
+`;
+
+const ModalText = styled.div`
+  color: white;
+  font-size: 18px;
+  margin-left: 5%;
+`;
+
+const ModalContent = styled.div`
+  width: 100%;
+  height: 90%;
+  padding: 5vh 0;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Cancel = styled(Image)`
+  width: 2.5vh;
+  height: 2.5vh;
+  margin: 0 1.5vh 0 auto;
+  cursor: pointer;
+`;
+
+export default DeleteModal;
