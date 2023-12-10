4b6da329bf6aaf3ff90e7b36b8cc209320ade3cb
5ewon06 <gwonsewon03@gmail.com>
2023-12-10 10:28:49 +0900

    Style: 상태 순서 변경

--- src/page/Main/layout/MemberListItem.tsx
+++ src/page/Main/layout/MemberListItem.tsx
@@ -3,31 +3,31 @@ import styled from "styled-components";
 import MemberListItemType from "../interface/memberListItemType.interface";
 
 const MemberListItem = ({ children }: MemberListItemType) => {
-    return (
-        <ListItemWrapper>
-            <MemberName>{children.name}</MemberName>
-            <div>
-                {children.place}, {children.howLong}, {children.what}
-            </div>
-        </ListItemWrapper>
-    );
+  return (
+    <ListItemWrapper>
+      <MemberName>{children.name}</MemberName>
+      <div>
+        {children.place}, {children.what}, {children.howLong}
+      </div>
+    </ListItemWrapper>
+  );
 };
 
 const ListItemWrapper = styled.div`
-    width: 100%;
-    height: 6vh;
-    background-color: #3e4249;
-    margin: 1vh auto;
-    border-radius: 5px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  width: 100%;
+  height: 6vh;
+  background-color: #3e4249;
+  margin: 1vh auto;
+  border-radius: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const MemberName = styled.div`
-    font-size: 20px;
-    width: 100%;
-    color: white;
+  font-size: 20px;
+  width: 100%;
+  color: white;
 `;
 
 export default MemberListItem;
