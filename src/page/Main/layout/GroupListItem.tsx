896d001654fbc35955549d837d2ba9cccda5f570
5ewon06 <gwonsewon03@gmail.com>
2023-12-10 10:27:34 +0900

    Feat: api 값 적용, 삭제 기능 추가

--- src/page/Main/layout/GroupListItem.tsx
+++ src/page/Main/layout/GroupListItem.tsx
@@ -3,48 +3,77 @@ import styled from "styled-components";
 import ListItemType from "../interface/groupListItemType.interface";
 import { useRecoilState } from "recoil";
 import currentGroupStore from "@/store/currentGroup.store";
+import Image from "next/image";
+import Kebab from "../assets/kebab.svg";
+import deleteModal from "@/store/deleteModal.store";
 
 const GroupListItem = ({ children }: ListItemType) => {
-    const [currentGroup, setCurrentGroup] = useRecoilState(currentGroupStore);
-    return (
-        <ListItemWrapper onChange={() => setCurrentGroup(children)}>
-            <GroupName type="radio" name="group" id={children} />
-            <GroupLabel htmlFor={children}>{children}</GroupLabel>
-        </ListItemWrapper>
-    );
+  const [currentGroup, setCurrentGroup] = useRecoilState(currentGroupStore);
+  const [isModal, setIsModal] = useRecoilState(deleteModal);
+
+  return (
+    <Container
+      onChange={() => setCurrentGroup({ id: children.id, name: children.name })}
+    >
+      <GroupInput type="radio" name="group" id={children.name} />
+      <GroupLabel htmlFor={children.name}>
+        <GroupName>
+          {children.name}({children.id})
+        </GroupName>
+        <GroupMenu
+          src={Kebab}
+          alt="GROUP_MENU_ICON"
+          onClick={() => setIsModal(true)}
+        />
+      </GroupLabel>
+    </Container>
+  );
 };
 
-const ListItemWrapper = styled.div`
-    width: 100%;
-    height: 6vh;
-    background-color: #3e4249;
-    margin: 1vh auto;
-    border-radius: 5px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+const Container = styled.div`
+  width: 100%;
+  height: 6vh;
+  background-color: #3e4249;
+  margin: 1vh auto;
+  border-radius: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const GroupLabel = styled.label`
-    width: 100%;
-    height: 100%;
-    position: relative;
-    border-radius: 5px;
+  width: 100%;
+  height: 100%;
+  position: relative;
+  border-radius: 5px;
+
+  display: flex;
 `;
 
-const GroupName = styled.input`
-    position: absolute;
-    font-size: 30px;
-    color: white;
-    visibility: hidden;
+const GroupInput = styled.input`
+  position: absolute;
+  font-size: 30px;
+  color: white;
+  visibility: hidden;
 
-    &:checked + label {
-        /* width: 100%;
-        height: 100%; */
-        border-radius: 5px;
-        background-color: #383b41;
-        color: white;
-    }
+  &:checked + label {
+    border-radius: 5px;
+    background-color: #383b41;
+    color: white;
+  }
+`;
+
+const GroupName = styled.div`
+  height: 1em;
+  line-height: 50%;
+  margin: auto 0;
+`;
+
+const GroupMenu = styled(Image)`
+  width: auto;
+  height: 4.5vh;
+  margin: 4% 0 0 auto;
+  cursor: pointer;
 `;
 
 export default GroupListItem;
